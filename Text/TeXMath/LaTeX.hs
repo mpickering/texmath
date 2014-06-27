15b7afb09fe60182a5d120bae0bcbdb6b79ce0fc
Matthew Pickering <matthewtpickering@gmail.com>
2014-06-27 11:43:59 +0100

    Added Space information to Shared.hs and changed LaTeX writer to appreciate this

--- Text/TeXMath/LaTeX.hs
+++ Text/TeXMath/LaTeX.hs
@@ -3,7 +3,8 @@ module Text.TeXMath.LaTeX (writeLaTeX) where
 import Text.TeXMath.Types
 import Data.List (intersperse)
 import Text.TeXMath.UnicodeToLaTeX
-
+import Text.TeXMath.Shared
+import qualified Data.Map as M
 
 writeLaTeX :: DisplayType -> [Exp] -> String
 writeLaTeX t es = concatMap writeExp es
@@ -12,10 +13,10 @@ writeExp :: Exp -> String
 writeExp (ENumber s) = getLaTeX s
 writeExp (EGrouped es) = concatMap writeExp es
 writeExp (EDelimited open close es) = getLaTeX open ++ concat (intersperse "," (map writeExp es)) ++ getLaTeX close
-writeExp (EIdentifier s) = getLaTeX s ++ " "
+writeExp (EIdentifier s) = "{" ++ getLaTeX s ++ "}"
 writeExp (EMathOperator s) = getLaTeX s
 writeExp (ESymbol _ s) = getLaTeX s
-writeExp (ESpace width) = ""
+writeExp (ESpace width) = getSpaceCommand width 
 writeExp (EBinary s e1 e2) = s ++ (inBraces e1) ++ (inBraces e2)
 writeExp (ESub b e1) = writeExp b ++ "_" ++ (inBraces e1) 
 writeExp (ESuper b e1) = writeExp b ++ "^" ++ (inBraces e1)  
@@ -28,12 +29,15 @@ writeExp (EDown b e1) = writeExp b ++ "_" ++ (inBraces e1)
 writeExp (EDownup b e1 e2) = writeExp b ++ "_" ++ (inBraces e1) ++ "^" ++ inBraces e2
 writeExp (EUnary s e) = s ++ inBraces e
 writeExp (EScaled size e) = ""
+writeExp (EStretchy (ESymbol Open e)) = "\\left" ++ getLaTeX e
+writeExp (EStretchy (ESymbol Close e)) = "\\right" ++ getLaTeX e
 writeExp (EStretchy e) = writeExp e
 writeExp (EArray aligns rows) = ""
-writeExp (EText ttype s) = ""
+writeExp (EText ttype s) = getLaTeXTextCommand ttype ++ "{"++s++"}"
 
 inBraces :: Exp -> String
 inBraces e = "{" ++ writeExp e ++ "}"
+
 
 
 
--- Text/TeXMath/Shared.hs
+++ Text/TeXMath/Shared.hs
@@ -1,33 +1,57 @@
 module Text.TeXMath.Shared 
-  (getMMLType, getTextType) where
+  (getMMLType, getTextType, getSpaceCommand, getLaTeXTextCommand) where
 
 
 import Text.TeXMath.Types
 import qualified Data.Map as M
 import Data.Maybe
+import Control.Applicative  
 
 getMMLType :: TextType -> String
-getMMLType t = fromMaybe "normal" (M.lookup t (M.fromList types))
+getMMLType t = fromMaybe "normal" (fst <$> M.lookup t (M.fromList types))
+
+getLaTeXTextCommand :: TextType -> String
+getLaTeXTextCommand t = fromMaybe "\\mathrm" (snd <$> M.lookup t (M.fromList types))
 
 
 getTextType :: String -> TextType
 getTextType s = fromMaybe TextNormal (M.lookup s revTypes)
   where
-    revTypes = M.fromList (map (\(k,v) -> (v,k)) types)
+    revTypes = M.fromList (map (\(k,v) -> (fst v,k)) types)
 
-types :: [(TextType, String)]
+getSpaceCommand :: String -> String 
+getSpaceCommand width = snd $ fromMaybe (M.findMax spaceMap) (M.lookupGE width spaceMap)
+  where 
+    spaceMap = M.fromList (map (\(k, ESpace s) -> (s, k)) spaceCommands)
+
+spaceCommands :: [(String, Exp)]
+spaceCommands = 
+           [ ("\\!", ESpace "-0.167em")
+           , (""   , ESpace "0.0em")
+           , ("\\,", ESpace "0.167em")
+           , ("\\>", ESpace "0.222em")
+           , ("\\:", ESpace "0.222em")
+           , ("\\;", ESpace "0.278em")
+           , ("~", ESpace "0.333em")
+           , ("\\quad", ESpace "1.0em")
+           , ("\\qquad", ESpace "2.0em")]
+
+
+--TextType to (MathML, LaTeX)
+types :: [(TextType, (String, String))]
 types = 
-  [ ( TextNormal       , "normal")
-  , ( TextBold         , "bold")
-  , ( TextItalic       , "italic")
-  , ( TextMonospace    , "monospace")
-  , ( TextSansSerif    , "sans-serif")
-  , ( TextDoubleStruck , "double-struck")
-  , ( TextScript       , "script")
-  , ( TextFraktur      , "fraktur")
-  , ( TextBoldItalic          , "bold-italic")
-  , ( TextBoldSansSerif       , "bold-sans-serif")
-  , ( TextBoldSansSerifItalic , "sans-serif-bold-italic")
-  , ( TextBoldScript          , "bold-script")
-  , ( TextBoldFraktur         , "bold-fraktur")
-  , ( TextSansSerifItalic     , "sans-serif-italic") ]
+  [ ( TextNormal       , ("normal", "\\mathrm"))
+  , ( TextBold         , ("bold", "\\mathbf"))
+  , ( TextItalic       , ("italic","\\mathit"))
+  , ( TextMonospace    , ("monospace","\\mathtt"))
+  , ( TextSansSerif    , ("sans-serif","\\mathsf"))
+  , ( TextDoubleStruck , ("double-struck","\\mathbb"))
+  , ( TextScript       , ("script","\\mathcal"))
+  , ( TextFraktur      , ("fraktur","\\mathfrak"))
+  , ( TextBoldItalic          , ("bold-italic","\\mathbfit"))
+  , ( TextBoldSansSerif       , ("bold-sans-serif","\\mathbfsfup"))
+  , ( TextBoldSansSerifItalic , ("sans-serif-bold-italic","\\mathbfsfit"))
+  , ( TextBoldScript          , ("bold-script","\\mathbfscr"))
+  , ( TextBoldFraktur         , ("bold-fraktur","\\mathbffrak"))
+  , ( TextSansSerifItalic     , ("sans-serif-italic","\\mathsfit")) ]
+
